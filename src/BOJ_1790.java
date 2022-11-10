189380118847029fa55a05d4cb19c1fa3c9a36c6
Faper <43310141+Seo-Faper@users.noreply.github.com>
2022-11-10 15:04:16 +0000

    단순 구현

--- src/BOJ_1790.java
+++ src/BOJ_1790.java
@@ -1,57 +1,29 @@
 import java.util.Scanner;
-/*
- * 
- * 
- * 12345689
 
-1의 자리일 땐 간격이 없음, 즉 k번째 수는 n과 동일
-
-101112 일 때, 
-
-10번째 수 : 1
-11번째 수 : 0 (1:0)
-
-12번째 수 : 1
-13번째 수 : 1 (3:1)
-
-14번째 수 : 1
-15번째 수:  2 (5:2)
-
-16번째 수 : 1
-17번째 수 : 3 (7:3)
-
- 
-18번째 수:  1
-19번째 수:  4 (9:4)
-
-20 : 1
-21 : 5
-
-22 : 1
-23 : 6
-
-24 : 1
-25 : 7
-
-26 : 1
-27 : 8
-
-28 : 1
-29: 9
-
-
-
-n이  10~19 일 때는  짝수 번째 수는 1, 홀수 번째 수는 (k-11) // 2 
-n이  20~29 일 때는?
-
-
- */
 public class BOJ_1790 {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         int n = sc.nextInt();
         int k = sc.nextInt();
+        
+        int find = 0;
+        int stack = 9;
+        int len = 1;
+        
+        while(true){
+            if (k <= len*stack) break;
+            k -= (len*stack);
+            find +=stack;
 
-
+            len++;
+            stack*=10;
+        }
+        find = (find+1) + (k-1)/len;
+        if(find > n) System.out.println(-1);
+        else{
+            int t = (int)((k-1)%len);
+            System.out.println((""+find).charAt(t));
+            
+        }
     }
 }
