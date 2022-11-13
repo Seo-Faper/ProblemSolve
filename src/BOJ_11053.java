d0ddce0bfb690f8959ae3c1ae84bad610d6ba276
Faper <43310141+Seo-Faper@users.noreply.github.com>
2022-11-13 15:06:31 +0000

    기초 생성

---
+++ src/BOJ_11053.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class BOJ_11053{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+    }
+}
