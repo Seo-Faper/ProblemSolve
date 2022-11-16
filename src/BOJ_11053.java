d63da5de2444b45fc17ed35a5de98ada7e190033
Faper <43310141+Seo-Faper@users.noreply.github.com>
2022-11-16 09:45:19 +0000

    가장 긴 증가하는 부분 수열

--- src/BOJ_11053.java
+++ src/BOJ_11053.java
@@ -4,19 +4,24 @@ public class BOJ_11053{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arr = new int[n];
+        int[] arr = new int[n+1];
         int[] dp = new int[n+1];
-        for(int i = 0; i<n; i++){
+        int ans = 1;
+
+        for(int i = 1; i<=n; i++){
             arr[i] = sc.nextInt();
         }   
-        dp[0] = 1;
-        for(int i = 1; i<n; i++){
-            if(arr[i] > arr[i-1]){
-                dp[i] += dp[i-1] + 1;
-            } 
-            else dp[i] = dp[i-1];
+        dp[1] = 1;
+        for(int i = 2; i<=n; i++){
+            dp[i] = 1;
+            for(int j = 1; j<i; j++){  
+                if(arr[i] > arr[j] && dp[i] <=dp[j]){
+                    dp[i] = dp[j]+1;
+                }
+            }
+            ans = Math.max(ans,dp[i]);
         }
-
+        System.out.println(ans);
 
     }
 }
