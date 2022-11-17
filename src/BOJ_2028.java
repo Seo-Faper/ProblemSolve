709901c86c5e061655e20238973f84e2fe443942
Faper <43310141+Seo-Faper@users.noreply.github.com>
2022-11-17 17:09:58 +0000

    solved

---
+++ src/BOJ_2028.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class BOJ_2028 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0; i<n; i++){
+            int tmp = sc.nextInt();
+        }
+    }
+}
