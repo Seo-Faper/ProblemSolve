3efb2854df4c5dc7b75a33a25317abf1431814b4
Faper <43310141+Seo-Faper@users.noreply.github.com>
2022-11-21 14:11:20 +0000

    백준 2407 조합

---
+++ src/BOJ_2407.java
@@ -0,0 +1,36 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BOJ_2407 {
+
+    public static BigInteger dpCombi[][];
+
+    public static void setDpCombination(int n, int m){
+
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=i;j++){
+                if(j == 0 || j==i)
+                    dpCombi[i][j] = new BigInteger("1");
+                else
+                    dpCombi[i][j] = dpCombi[i-1][j-1].add(dpCombi[i-1][j]);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception{
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        dpCombi = new BigInteger[1001][1001];
+
+        setDpCombination(n, m);
+
+        System.out.println(dpCombi[n][m]);
+
+    }
+
+}
